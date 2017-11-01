e8cf8553a4c54bf6a0c821425cacc9ea9cdec933
zhuyund <zhuyund@cs.cmu.edu>
2017-11-01 17:40:32 -0400

    initial commit

---
+++ deeplearning4ir/tools/gen_trec_from_score.py
@@ -0,0 +1,33 @@
+import argparse
+import sys
+reload(sys)
+sys.setdefaultencoding('UTF8')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("score_file", help="K-NRM output score file", type=argparse.FileType('r'))
+    parser.add_argument("trec_org_file", help="must in the same order as score_file", type=argparse.FileType('r'))
+    parser.add_argument("out_file", type=argparse.FileType('w'))
+    args = parser.parse_args()
+
+    # read score and trec format
+    res = []
+    for score, trec_line in zip(args.score_file, args.trec_org_file):
+        score = float(score)
+        items = trec_line.strip().split('\t')
+        qid = items[0]
+        docid = items[2]
+
+        res.append((qid, score, docid))
+
+    res = sorted(res, reverse=True)
+    prev_qid = -1
+    rank = 0
+    for qid, score, docid in res:
+        if prev_qid != qid:
+            prev_qid = qid
+            rank = 0
+        rank += 1
+        args.out_file.write("{0}\tQ0\t{1}\t{2}\t{3}\tknrm\n".format(qid, docid, rank, score))
+
+
