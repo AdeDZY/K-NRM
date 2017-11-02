bcf4ad7c0b4389b6353f5062b7c38f1b87e39b6a
Ubuntu <ubuntu@ip-172-31-61-254.ec2.internal>
2017-11-02 03:26:04 +0000

    fixed sample train scripts

--- train-sogou-knrm.sh
+++ train-sogou-knrm.sh
@@ -1,1 +1,5 @@
-python ./deeplearning4ir/clicknn/knrm.py sogou.knrm.config --train -f ../data1/train-dev/pad-title-url/train.DCTR.pairs.hashed.shuf -v ../data1/train-dev/pad-title-url/dev.pairs.hashed.shuf -z 8553124 -s ./output/test/
+python ./deeplearning4ir/clicknn/knrm.py sogou.knrm.config --train \
+      --train_file "../data1/train-dev/pad-title-url/train.DCTR.pairs.hashed.shuf" \
+      --validation_file "../data1/train-dev/pad-title-url/dev.pairs.hashed.shuf" \
+      --train_size 8553124 \
+      --checkpoint_dir "./output/test/" 
